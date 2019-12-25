8a6d985d62bf88358ef1c54c3615462128bf36e6
Edward Loveall <edward@thoughtbot.com>
2019-12-25 15:35:15 -0500

    Optimize a little
    Removing the two_item_permutation method because the algorithm can
    figure out 2-item sets without a special case.

--- permutation.rb
+++ permutation.rb
@@ -16,8 +16,6 @@ class Permutation
     permutation_count = Math.gamma(set.size + 1).to_i
     if set.size == 1
       set
-    elsif set.size == 2
-      two_item_permutation(set, offset)
     else
       wrapped_offset = offset % permutation_count
       section_size = Math.gamma(set.size).to_i
@@ -28,14 +26,6 @@ class Permutation
       new_set = set.dup
       new_set.delete_at(current_section)
       result + permutation_at_offset(new_set, wrapped_offset)
-    end
-  end
-
-  def two_item_permutation(set, offset)
-    if offset.even?
-      set
-    else
-      [set[1], set[0]]
     end
   end
 end
