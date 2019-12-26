6edef83afcf62972c034dafed492f12c2a92ee44
Edward Loveall <edward@thoughtbot.com>
2019-12-26 13:41:14 -0500

    Refactor algo to use scoped offsets
    This works a bit differently than the previous version. It calculates
    the index of the item that goes in the first slot for a theoretical set
    of a certain length based on the offset. Then calculates the index for
    a set that has one less element, and so on until it does it for a
    2-item set. Once it has all the indexes, it loops through them and
    extracts each item by its index from the original set, leaving us with
    one element left over which it adds to the end.

--- permutation.rb
+++ permutation.rb
@@ -12,21 +12,14 @@ class Permutation
   private
 
   def permutation_at_offset(set, offset)
-    if set.size == 1
-      set
-    else
-      result = []
-      permutation_count = Math.gamma(set.size + 1).to_i
+    indexes = set.size.downto(2).map do |size|
+      permutation_count = factorial(size)
       wrapped_offset = offset % permutation_count
-      section_size = Math.gamma(set.size).to_i
-      current_section = wrapped_offset / section_size
-      focus_item = set[current_section]
-
-      result[0] = focus_item
-      new_set = set.dup
-      new_set.delete_at(current_section)
-      result + permutation_at_offset(new_set, wrapped_offset)
+      section_size = factorial(size - 1)
+      wrapped_offset / section_size
     end
+    new_set = set.dup
+    indexes.map { |i| new_set.delete_at(i) } + new_set
   end
 
   def factorial(n)
