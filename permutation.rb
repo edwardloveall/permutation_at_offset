3e95452faa9eba721912379dd56f784e26d37ac0
Edward Loveall <edward@edwardloveall.com>
2020-01-04 10:31:25 -0500

    Don't skip the single-item permutation
    It's easy to figure out what the first item will be for a single item
    permutation so I was skipping it. But I think it makes the code a touch
    easier to understand and doesn't affect speed in any meaningful way.

--- permutation.rb
+++ permutation.rb
@@ -14,14 +14,14 @@ class Permutation
   private
 
   def permutation_at_offset(set, offset)
-    indexes = set.size.downto(2).map do |size|
+    indexes = set.size.downto(1).map do |size|
       permutation_count = factorial(size)
       wrapped_offset = offset % permutation_count
       section_size = factorial(size - 1)
       wrapped_offset / section_size
     end
     new_set = set.dup
-    indexes.map { |i| new_set.delete_at(i) } + new_set
+    indexes.map { |i| new_set.delete_at(i) }
   end
 
   def factorial(int)
